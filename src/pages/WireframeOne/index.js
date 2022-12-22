8c0b7fd6bb35c82a7ef418544295920793326040
migueldiazmacedo <miguel.diazmacedo@gmail.com>
2022-12-22 21:48:37 +0000

    Code Synced by DhiWise

---
+++ src/pages/WireframeOne/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WireframeOnePage = () => {
+  return (
+    <>
+      <div className="bg-white_A700 h-[1024px] sm:h-[546px] md:h-[705px] mx-[auto] w-[100%]"></div>
+    </>
+  );
+};
+
+export default WireframeOnePage;
